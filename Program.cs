041c39d9161ed0b5ca27b8812d43920d708019fb
blakSou1 <142041470+blakSou1@users.noreply.github.com>
2023-09-19 22:36:28 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,8 +1,23 @@
-﻿Console.WriteLine("4 плюс 6 = " + (4 + 6));
-int s = 4 - 1;
-Console.WriteLine("4 уменьшеное на 1 = " + s);
-Console.WriteLine("3 умножить на 7 = " + 3 * 7);
-Console.WriteLine("16 разделить на 8 = " + 16 / 8);
-Console.WriteLine("11 разделить на 5 = " + 11 / 5.0);
-Console.WriteLine("целая часть от деления 13 на 3 = " + 13 / 3);
-Console.WriteLine("остаток от деления 17 на 8 = " + 17 % 8);
+﻿string s = "Каждый ";
+string s1 = "охотник ";
+string s2 = "желает ";
+string s3 = "знать ";
+string s4 = "где ";
+string s5 = "сидит ";
+string s6 = "фазан.";
+
+Console.Write(s);
+Console.Write(s1);
+Console.Write(s2);
+Console.Write(s3);
+Console.Write(s4);
+Console.Write(s5);
+Console.WriteLine(s6);
+
+string concatString = s + "охотник " + s2 + "знать " + s4 + "сидит " + s6;
+
+Console.WriteLine(concatString);
+
+string interpolatedString = $"{s} {s1} {s2} {s3} {s4} {s5} {s6}";
+
+Console.WriteLine(interpolatedString);

ec0c8e25bb4f4ddfa54e0e29509b7d833141cfb0
blakSou1 <142041470+blakSou1@users.noreply.github.com>
2023-09-19 16:11:51 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,23 +1,7 @@
-﻿int age;
-string name = "Вячеслав";
-
-string programCount;
-string money;
-int time;
-bool truth;
-string progressiv;
-
-time = 1;
-name = "Вячеслав";
-money = "6 значную сумму";
-programCount = "1";
-age = 17;
-truth = true;
-progressiv = "?";
-
-Console.WriteLine("Здаров, обращайтесь ко мне" + name + ".");
-Console.WriteLine("Мне" + age + "лет.");
-Console.WriteLine("Я хочу научится делать игры" + progressiv + "это моя" + programCount + "программа на языке С#.");
-Console.WriteLine("Кгда я закончу обучение я хочу зарабатывать" + money + "денег в " + time + "наносекунд.");
-Console.WriteLine("Достигну ли я желаемого" + progressiv + truth + "!");
-Console.ReadKey();
+﻿char f = 'a';
+string e = "World";
+float d = 65.46f;
+int c = 55555;
+byte a = 222;
+short b = -99;
+uint u = 2394672295;

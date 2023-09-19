a08c9c95e29b7cef79e60cdd81780f5b0a201dd9
blakSou1 <142041470+blakSou1@users.noreply.github.com>
2023-09-19 19:44:54 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 int s = 4 - 1;
 Console.WriteLine("4 уменьшеное на 1 = " + s);
 Console.WriteLine("3 умножить на 7 = " + 3 * 7);
-Console.WriteLine("16 разделить на 8 = " + 18 / 8);
-Console.WriteLine("11 разделить на 5 = " + 11 / 5);
+Console.WriteLine("16 разделить на 8 = " + 16 / 8);
+Console.WriteLine("11 разделить на 5 = " + 11 / 5.0);
 Console.WriteLine("целая часть от деления 13 на 3 = " + 13 / 3);
 Console.WriteLine("остаток от деления 17 на 8 = " + 17 % 8);

e10d506c1395926d1307f90a87420ae3973e2773
blakSou1 <142041470+blakSou1@users.noreply.github.com>
2023-09-20 20:41:36 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,9 +1,40 @@
-﻿Console.Write("Введите ваше имя");
-string name = Console.ReadLine();
+﻿using System.Runtime.CompilerServices;
 
-Console.WriteLine();
-Console.Write("Введите свой возраст");
-string age = Console.ReadLine();
+int magicNumber = 4;
 
-Console.WriteLine();
-Console.Write($"Благодарим {name} ({age}) за введенную инфрмацию! Очень важный спам будет отправлятся вам на почту!");
+Con();
+
+void Con()
+{
+
+    Console.WriteLine("Угадай число");
+    Console.WriteLine("");
+    Console.WriteLine("Загадано число от 1 до 5. попробуйте его отгадать!");
+    Console.WriteLine();
+    Console.Write("Введите ваше число: ");
+
+    int input = Convert.ToInt32(Console.ReadLine());
+
+    if(magicNumber == input)
+    {
+
+        Console.WriteLine("Да! Это число" + magicNumber + "!");
+
+    }
+    else
+    {
+
+        Console.WriteLine("Нет! Это не" + input + "!");
+        Console.WriteLine("попоробуете еще раз?");
+        Console.WriteLine("чтобы закончить нажмите А");
+
+        if (Console.ReadLine() != "A")
+        {
+
+                magicNumber = new Random().Next(0, 5);
+
+        }
+
+    }
+
+}

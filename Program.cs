776b62be9964e91cb47d804a83c7ecb21d1836f1
blakSou1 <142041470+blakSou1@users.noreply.github.com>
2023-09-20 15:20:07 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,23 +1,5 @@
-﻿string s = "Каждый ";
-string s1 = "охотник ";
-string s2 = "желает ";
-string s3 = "знать ";
-string s4 = "где ";
-string s5 = "сидит ";
-string s6 = "фазан.";
+﻿string stringNumber = "256";
 
-Console.Write(s);
-Console.Write(s1);
-Console.Write(s2);
-Console.Write(s3);
-Console.Write(s4);
-Console.Write(s5);
-Console.WriteLine(s6);
-
-string concatString = s + "охотник " + s2 + "знать " + s4 + "сидит " + s6;
-
-Console.WriteLine(concatString);
-
-string interpolatedString = $"{s} {s1} {s2} {s3} {s4} {s5} {s6}";
-
-Console.WriteLine(interpolatedString);
+int numberFromString = Convert.ToInt32(stringNumber);
+int square = numberFromString * numberFromString;
+Console.WriteLine("Квадрат введенного числа равен " + square);

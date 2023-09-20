c306599f8218027e08921b472430ed12ef475704
blakSou1 <142041470+blakSou1@users.noreply.github.com>
2023-09-20 16:02:05 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿Console.Write("Введите ваше имя");
 string name = Console.ReadLine();
 
-Console.WriteLine("Введите свой возраст");
+Console.WriteLine();
+Console.Write("Введите свой возраст");
 string age = Console.ReadLine();
 
-Console.WriteLine($"Благодарим {name} ({age}) за введенную инфрмацию! Очень важный спам будет отправлятся вам на почту!");
+Console.WriteLine();
+Console.Write($"Благодарим {name} ({age}) за введенную инфрмацию! Очень важный спам будет отправлятся вам на почту!");
